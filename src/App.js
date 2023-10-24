0392eafc0430ababae93452401e4e19944c2e253
Tyler Knowler <knowlertyler@gmail.com>
2023-10-24 20:43:23 +0100

    removed projects (routes)

--- src/App.js
+++ src/App.js
@@ -4,8 +4,6 @@ import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import './css/index.css';
 
 import Home from './Home.js';
-import Calc from './components/calc/calc.js';
-import LoLCalc from './components/lolcalc/lolcalc.js';
 
 
 function App() {
@@ -14,8 +12,6 @@ function App() {
       <div>
         <Routes>
           <Route exact path="/"  element={<Home/>} />
-          <Route path="/calculator"  element={<Calc/>} />
-          <Route path="/leaguecalc"  element={<LoLCalc/>} />
         </Routes>
       </div>
     </Router>
