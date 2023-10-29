0540b8fdff2588f88066dfceed4a6a5231e2437a
Tyler Knowler <knowlertyler@gmail.com>
2023-10-29 10:19:14 +0000

    changed minor visuals and added a little about me

--- src/components/About.js
+++ src/components/About.js
@@ -6,8 +6,15 @@ return (
         <h1>About Me</h1>
         <div className='about-me-flex'>
             <div className="about-me-box about-me-personal">
-                <div className="table-top personal-top"><p>About Me</p></div>
-                <div className="table-content personal-content"></div>
+                <div className="table-top personal-top"><p>Facts About Me</p></div>
+                <div className="table-content personal-content">
+                    <p>
+                        I am an avid gamer<br></br>
+                        I have a cat called Jeffrey<br></br>
+                        I studied guitar for 7 years<br></br>
+                        I studied piano for 2 years<br></br>
+                    </p>
+                </div>
             </div>
             <div className="about-me-box about-me-front">
                 <div className="table-top front-top"><p>Front End Skills</p></div>
